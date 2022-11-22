b20513ef9446bc56562863114980a8a97f46d01d
Vitor <vitor.22sk@gmail.com>
2022-11-22 20:47:37 -0300

    update requirements

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -13,12 +13,6 @@ def get_sentiments(commentary):
         commentary.sentiment_type = key
         commentary.sentiment_score = value
 
-        # if (value >= 0.05):
-        #     commentary.sentiment = 'positive'
-        # elif (value > -0.05) and (value < 0.05):
-        #     commentary.sentiment = 'neutral'
-        # elif (value <= 0.05):
-        #     commentary.sentiment = 'negative'
         sentiments.append(commentary)
 
     # so deixa as linhas onde o sentimento é compound
@@ -28,40 +22,3 @@ def get_sentiments(commentary):
 
 
     return { "Mensagem": "ola" }
-
-
-
-
-## usar isso?
-# def remove_noise(tweet_tokens, stop_words = ()):
-def remove_noise(sentece):
-
-    # removido caracteres não alfabeticos
-    sentece = sentece.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
-
-    #remove caracteres unicode
-    sentece = sentece.replace(r'\r+|\n+|\t+','', regex=True)
-
-    # covertido para lower-case
-    sentece = sentece.str.casefold()
-
-    # cleaned_tokens = []
-
-    # for token, tag in pos_tag(tweet_tokens):
-    #     token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
-    #                    '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
-    #     token = re.sub("(@[A-Za-z0-9_]+)","", token)
-
-    #     if tag.startswith("NN"):
-    #         pos = 'n'
-    #     elif tag.startswith('VB'):
-    #         pos = 'v'
-    #     else:
-    #         pos = 'a'
-
-    #     lemmatizer = WordNetLemmatizer()
-    #     token = lemmatizer.lemmatize(token, pos)
-
-    #     if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
-    #         cleaned_tokens.append(token.lower())
-    return cleaned_tokens
